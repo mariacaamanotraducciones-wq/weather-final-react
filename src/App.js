5fc288d997601462653cc0127a4ce02b832abbde
mariacaamanotraducciones-wq <maria.caamano.traducciones@gmail.com>
2026-08-01 13:41:38 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Weather app</h1>
-      <footer>
-        Coded by Maria Caamano and is {""}
-        <a
-          href="https://github.com/mariacaamanotraducciones-wq/weather-final-react"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Open-sourced
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather app</h1>
+        <footer>
+          Coded by Maria Caamano and is {""}
+          <a
+            href="https://github.com/mariacaamanotraducciones-wq/weather-final-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-sourced
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
