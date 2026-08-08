4e145ff55feaffab15d9a90e851d7974837f90a7
mariacaamanotraducciones-wq <maria.caamano.traducciones@gmail.com>
2026-08-08 12:02:31 +0200

    Added API real data and loop through it

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,54 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./WeatherForecast.css";
-export default function WeatherForecast() {
-  return (
-    <div className="WeatherForecast">
-      <div className="row">
-        <div className="col">
-          <div className="forecast-day">Mon</div>
-          <div className="forecast-icon">☀️</div>
-          <div className="forecast-temperatures">
-            <span className="forecast-temperature-max">19°</span>
-            <span className="forecast-temperature-min">10°</span>
-          </div>
-        </div>
 
-        <div className="col">
-          <div className="forecast-day">Tue</div>
-          <div className="forecast-icon">🌤️</div>
-          <div className="forecast-temperatures">
-            <span className="forecast-temperature-max">18°</span>
-            <span className="forecast-temperature-min">11°</span>
-          </div>
-        </div>
+export default function WeatherForecast(props) {
+  const [loaded, setLoaded] = useState(false);
+  const [forecast, setForecast] = useState([]);
+  const [currentCity, setCurrentCity] = useState("");
 
-        <div className="col">
-          <div className="forecast-day">Wed</div>
-          <div className="forecast-icon">🌧️</div>
-          <div className="forecast-temperatures">
-            <span className="forecast-temperature-max">15°</span>
-            <span className="forecast-temperature-min">9°</span>
-          </div>
-        </div>
+  if (props.city !== currentCity) {
+    setCurrentCity(props.city);
+    setLoaded(false);
+  }
 
-        <div className="col">
-          <div className="forecast-day">Thu</div>
-          <div className="forecast-icon">🌩️</div>
-          <div className="forecast-temperatures">
-            <span className="forecast-temperature-max">14°</span>
-            <span className="forecast-temperature-min">8°</span>
-          </div>
-        </div>
+  function handleResponse(response) {
+    setForecast(response.data.daily);
+    setLoaded(true);
+  }
 
-        <div className="col">
-          <div className="forecast-day">Fri</div>
-          <div className="forecast-icon">☀️</div>
-          <div className="forecast-temperatures">
-            <span className="forecast-temperature-max">21°</span>
-            <span className="forecast-temperature-min">12°</span>
-          </div>
+  function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    return days[date.getDay()];
+  }
+
+  if (loaded) {
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          {/* Loop through the first 5 days of the forecast array */}
+          {forecast.map(function (dailyForecast, index) {
+            if (index < 5) {
+              return (
+                <div className="col" key={index}>
+                  <div className="forecast-day">
+                    {formatDay(dailyForecast.time)}
+                  </div>
+                  <img
+                    src={dailyForecast.condition.icon_url}
+                    alt={dailyForecast.condition.description}
+                    className="forecast-icon"
+                    width="42"
+                  />
+                  <div className="forecast-temperatures">
+                    <span className="forecast-temperature-max">
+                      {Math.round(dailyForecast.temperature.maximum)}°
+                    </span>
+                    <span className="forecast-temperature-min">
+                      {Math.round(dailyForecast.temperature.minimum)}°
+                    </span>
+                  </div>
+                </div>
+              );
+            } else {
+              return null;
+            }
+          })}
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "03e34f234b90fab95bd92od60988t445";
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${props.city}&key=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
+
+    return null; // Render nothing while fetching to avoid layout jumps
+  }
 }
